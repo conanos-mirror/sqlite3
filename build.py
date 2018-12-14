0e8968a4f4851576c0bfad9aae59d0b5f59185bb
cppbitman <daihongjun@kedacom.com>
2018-12-14 10:20:19 +0800

    windows build done

--- build.py
+++ build.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-from bincrafters import build_template_default
+from conanos.build import Main
 
 if __name__ == "__main__":
-
-    builder = build_template_default.get_builder()
-
-    builder.run()
-    
+    Main('sqlite3',pure_c=True)
